73be4c5159ddb84a9cb36d5d41f55710b7605d75
Henrique Lopes <riquellopes@gmail.com>
2014-10-01 22:47:11 -0300

    Modifying app file.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+import datetime
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restless import APIManager
@@ -19,18 +20,20 @@ class Product(db.Model):
 	price_total = None
 	price_cost = None
 	markup = None
+	date_created = db.Column(db.DateTime, default=datetime.now)
+	date_last_update = db.Column(db.DateTime, onupdate=datetime.now)
 	status = None
 	
 	def __repr__(self):
 		return "{0} R$ {1}".format(self.name, self.price_total)
 
 class User(db.Model):
-	id = None
-	name = None
-	email = None
-	profile_url = None
-	access_token = None
-	date_created = None
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200))
+	email = db.Column(db.String(120), unique=True)
+	profile_url = db.Column(db.String(200))
+	access_token = db.Column(db.String(200))
+	date_created = db.Column(db.DateTime, default=datetime.now)
 		
 manager = APIManager(app, flask_sqlalchemy_db=db)
 manager.create_api(Product, methods=['GET', 'POST', 'PUT'])
