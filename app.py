d87874f5580050928f9d8e0a3fd5f69036446523
Henrique Lopes <riquellopes@gmail.com>
2014-10-02 22:57:31 -0300

    Add news fields

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 
 class Photo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
 	label = db.Column(db.String(100))
 	path = db.Column(db.String(200))
 	
@@ -17,15 +18,16 @@ class Product(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100))
 	descriptions = db.Column(db.String(400))
-	price_total = None
-	price_cost = None
-	markup = None
+	price_total = db.Column(db.Float)
+	price_cost = db.Column(db.Float)
+	markup = db.Column(db.Float)
 	date_created = db.Column(db.DateTime, default=datetime.now)
 	date_last_update = db.Column(db.DateTime, onupdate=datetime.now)
-	status = None
+	status = db.Column(db.Integer, default=0)
+	photos = db.relationship('Photo', backref=db.backref('product', lazy='dynamic'))
 	
 	def __repr__(self):
-		return "{0} R$ {1}".format(self.name, self.price_total)
+		return "{0} R$ {1} - status: {2}".format(self.name, self.price_total, self.status)
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
